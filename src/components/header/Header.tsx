cf8ea80d8e01e1ce3369f3d8c1f8304f0f936c21
matheus mozart <mmsnborges@gmail.com>
2023-12-16 20:59:46 -0400

    desafiando-se

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,29 @@
-import Link from "next/link";
+"use client"
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./styles.module.scss";
-import Logo from '../../../public/images/logo.jpg'
-//import { FaAlignJustify } from "react-icons/fa";<FaAlignJustify />
+import Logo from '../../../public/images/pokémon_logo.svg'
+import { FaAlignJustify } from "react-icons/fa";<FaAlignJustify />
 
 export default function Header(){
+  const [navActive, setNavActive ] = useState('menu')
+  const  navToggler =  () => {
+    navActive === 'menu' ? setNavActive('menu nav_menu_responsive') : setNavActive('menu') ;
+  }
   return(
-    <header className={styles.headerContainer}>
-      <section className={styles.headerContent}>
-        <div className={styles.logo}>
-          <Link href={'/'}>
-            <Image src={Logo} alt="Essa casa até que é legal"/>
-          </Link>
-        </div>
-      
-      <nav className={styles.menu}>
-        
-         <ul>
-            <li><a href="#">Página Inicial</a></li>
-            <li><a href="#">Sobre Nós</a></li>
-            <li><a href="#">Serviços</a></li>
-            <li><a href="#">Contato</a></li>
+    <nav className={styles.nav_container}>
+      <a href="#" className={styles.brand}>Pokemon</a>
+    
+      <ul className={navActive}>
+        <li className={styles.nav_item}><a href="" className="navlink">Home</a></li>
+        <li className={styles.nav_item}><a href="" className="navlink">About</a></li>
+        <li className={styles.nav_item}><a href="" className="navlink">Skills</a></li>
+        <li className={styles.nav_item}><a href="" className="navlink">Portfólio</a></li>
+        <li className={styles.nav_item}><a href="" className="navlink">Contato</a></li>
       </ul>
-        </nav>
-      </section>
-
-    </header>
+      
+      <FaAlignJustify className={styles.toogle} onClick = {navToggler}/>
+    </nav>
  
   
   )
